5c9aa81c1f631c9f134ee529e827d6041f9910c5
Tryggvi Már Sigurjónsson <tryggvims@hotmail.com>
2017-04-23 20:01:44 +0000

    Jquery tilbúið

--- jscript.js
+++ jscript.js
@@ -3,37 +3,44 @@ $(document).ready(function(){
 	var fylki = [];
 	var breyta;
 	var breyta2;
-	var text;
-	var teljari = "2";
-	$('select[name=avoxtur]').change(function() {
-		breyta = $(this).val();
+	var pontun;
+	$('select[name=magn]').change(function() {
+		breyta2 = $(this).val();
 	});
 	$('select[name=avoxturNafn]').change(function() {
-		breyta2 = $(this).val();
-		});
+		breyta = $(this).val();
+	});
 	$('#hnappur1').click(function(){
-			fylki.unshift(breyta);
-			fylki.unshift(breyta2);
-			text = "<ul>";
+		if (breyta == null || breyta2 == null) {
+			alert("Veldu bæði tegund og magn1");
+		}
+		else if (breyta2 == "ekkert2") {
+			alert("Veldu bæði tegund og magn2");
+			breyta2 = null;
+		}
+		else if (breyta == "ekkert") {
+			alert("Veldu bæði tegund og magn3");
+			breyta = null;
+		}
+		else {
+			fylki.unshift(breyta , breyta2);
+			pontun = "<ul>";
 	   		for (i = 0; i < fylki.length; i+=2) {
-		   	 	text += "<li class="+i+">" + fylki[i] +" -- "+ fylki[i+1] +"<input type='button' class='eydaPontun' value='Eyða'/>"  +"</li>";
+		   	 	pontun += "<li class=" + i + ">" + fylki[i] + " -- " + fylki[i+1] + "<input type='button' class='eydaPontun' value='Eyða'/>" + "</li>";
 			}
-			text += "</ul>";
-			document.getElementById("pontunListi").innerHTML = text;
+			pontun += "</ul>";
+			document.getElementById("pontunListi").innerHTML = pontun;
 			$('.eydaPontun').on('click',function()
 			{
   				$(this).parent('li').remove();
 				var myClass = $(this).parent('li').attr("class");					
 				var mynumber = parseInt(myClass);
-  				fylki.splice((mynumber));
-  				fylki.splice(mynumber+1);
+  				fylki.splice(mynumber, 2);
 			});
-
+		}
 	});
 	$('#hnappur2').click(function(){
-				console.log(fylki);
+		console.log(fylki);
 
 	});
-//http://stackoverflow.com/questions/17313585/how-to-remove-a-specific-div-from-divs-with-same-class-name
-
 });
